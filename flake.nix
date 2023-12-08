6c4cde29527217e4b99d62525f75d327e0609bee
bodymindarts <justin@galoy.io>
2023-12-08 11:57:41 +0100

    chore: add flake.nix

---
+++ flake.nix
@@ -0,0 +1,65 @@
+{
+  description = "Cala";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+    rust-overlay = {
+      url = "github:oxalica/rust-overlay";
+      inputs = {
+        nixpkgs.follows = "nixpkgs";
+        flake-utils.follows = "flake-utils";
+      };
+    };
+  };
+  outputs = {
+    self,
+    nixpkgs,
+    flake-utils,
+    rust-overlay,
+  }:
+    flake-utils.lib.eachDefaultSystem
+    (system: let
+      overlays = [(import rust-overlay)];
+      pkgs = import nixpkgs {
+        inherit system overlays;
+      };
+      rustVersion = pkgs.pkgsBuildHost.rust-bin.fromRustupToolchainFile ./rust-toolchain.toml;
+      rustToolchain = rustVersion.override {
+        extensions = ["rust-analyzer" "rust-src"];
+      };
+      nativeBuildInputs = with pkgs;
+        [
+          rustToolchain
+          alejandra
+          sqlx-cli
+          cargo-nextest
+          cargo-audit
+          cargo-watch
+          postgresql
+          docker-compose
+          bats
+          jq
+        ]
+        ++ lib.optionals pkgs.stdenv.isDarwin [
+          darwin.apple_sdk.frameworks.SystemConfiguration
+        ];
+      devEnvVars = rec {
+        PGDATABASE = "pg";
+        PGUSER = "user";
+        PGPASSWORD = "password";
+        PGHOST = "127.0.0.1";
+        DATABASE_URL = "postgres://${PGUSER}:${PGPASSWORD}@${PGHOST}:5432/pg";
+        PG_CON = "${DATABASE_URL}";
+      };
+    in
+      with pkgs; {
+        devShells.default = mkShell (devEnvVars
+          // {
+            inherit nativeBuildInputs;
+          });
+
+        formatter = alejandra;
+      });
+}
+
