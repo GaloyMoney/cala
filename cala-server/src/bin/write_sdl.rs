a7f247bb82c694eebae0e5c935a7028963e8d70f
bodymindarts <justin@galoy.io>
2023-12-13 11:00:41 +0100

    chore: remove federation directives

--- cala-server/src/bin/write_sdl.rs
+++ cala-server/src/bin/write_sdl.rs
@@ -1,10 +1,3 @@
-use async_graphql::SDLExportOptions;
-
 fn main() {
-    println!(
-        "{}",
-        cala_server::graphql::schema(None)
-            .sdl_with_options(SDLExportOptions::new().federation())
-            .trim()
-    );
+    println!("{}", cala_server::graphql::schema(None).sdl().trim());
 }
