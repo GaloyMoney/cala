7708533dd6879f1cead9013f49967073186540e3
sandipndev <hey@sandipan.dev>
2024-05-15 15:24:41 +0530

    ci: publish to crates

---
+++ ci/tasks/publish-to-crates.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+set -e
+
+pushd repo
+
+cat <<EOF | cargo login
+${CRATES_API_TOKEN}
+EOF
+
+cargo publish -p cala-cel-parser --all-features --no-verify
+cargo publish -p cala-cel-interpreter --all-features --no-verify
+cargo publish -p cala-core-types --all-features --no-verify
+cargo publish -p cala-ledger-outbox-client --all-features --no-verify
+cargo publish -p cala-ledger --all-features --no-verify
+cargo publish -p cala-server --all-features --no-verify
