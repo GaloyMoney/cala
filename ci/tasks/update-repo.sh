1608251b187d6aac948a10ca05aed712a80dd1e4
Sandipan <sandipandey.12@gmail.com>
2024-05-17 20:16:40 +0530

    ci: wiring up all cargo packages for versioning before releasing (#56)
    * ci: wiring up all cargo packages for versioning before releasing
    
    * chore: adding descriptions and dev vesrsions to cargo toml
    
    * chore: update cargo lockfile
    
    * cargo yaml updat
    
    * test

--- ci/tasks/update-repo.sh
+++ ci/tasks/update-repo.sh
@@ -19,6 +19,21 @@ $(cat CHANGELOG.md)
 EOF
 mv new_change_log.md CHANGELOG.md
 
+for file in $(find . -mindepth 2 -name Cargo.toml); do
+  sed -i'' "s/^version.*/version = \"${VERSION}\"/" ${file}
+done
+
+sed -i'' "s/cel-parser\", version = .*/cel-parser\", version = \"${VERSION}\" }/" cala-cel-interpreter/Cargo.toml
+sed -i'' "s/cel-interpreter\", version = .*/cel-interpreter\", version = \"${VERSION}\" }/" cala-ledger-core-types/Cargo.toml
+sed -i'' "s/core-types\", version = .*/core-types\", version = \"${VERSION}\" }/" cala-ledger-outbox-client/Cargo.toml
+sed -i'' "s/cel-interpreter\", version = .*/cel-interpreter\", version = \"${VERSION}\" }/" cala-ledger-outbox-client/Cargo.toml
+sed -i'' "s/core-types\", version = .*/core-types\", version = \"${VERSION}\" }/" cala-ledger/Cargo.toml
+sed -i'' "s/cel-interpreter\", version = .*/cel-interpreter\", version = \"${VERSION}\" }/" cala-ledger/Cargo.toml
+sed -i'' "s/core-types\", version = .*/core-types\", version = \"${VERSION}\" }/" cala-server/Cargo.toml
+sed -i'' "s/cala-ledger\", version = .*/cala-ledger\", version = \"${VERSION}\" }/" cala-server/Cargo.toml
+sed -i'' "s/cel-interpreter\", version = .*/cel-interpreter\", version = \"${VERSION}\" }/" cala-server/Cargo.toml
+sed -i'' "s/cala-ledger-outbox-client\", version = .*/cala-ledger-outbox-client\", version = \"${VERSION}\" }/" cala-server/Cargo.toml
+
 git status
 git add .
 
