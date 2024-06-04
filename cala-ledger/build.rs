1efa9683fec6874ab8ae2da32c2b529ab9890665
bodymindarts <justin@galoy.io>
2024-06-04 12:18:10 +0200

    build: SQLX_OFFLINE=true for docs.rs

--- cala-ledger/build.rs
+++ cala-ledger/build.rs
@@ -1,4 +1,9 @@
 fn main() -> Result<(), Box<dyn std::error::Error>> {
+    if std::env::var_os("DOCS_RS").is_some() {
+        // When building in docs.rs, we want to set SQLX_OFFLINE mode to true
+        println!("cargo:rustc-env=SQLX_OFFLINE=true");
+    }
+
     println!("cargo:rerun-if-changed=migrations");
 
     std::env::set_var("PROTOC", protobuf_src::protoc());
