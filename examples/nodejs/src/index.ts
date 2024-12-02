fb6f18caeffe3e40bf974eda08a757eda77fa7d3
Vaibhav <thevaibhavdixit@gmail.com>
2024-12-02 19:12:56 +0530

    style: fix linting

--- examples/nodejs/src/index.ts
+++ examples/nodejs/src/index.ts
@@ -4,26 +4,26 @@ const main = async () => {
   const pgHost = process.env.PG_HOST || "localhost";
   const pgCon = `postgres://user:password@${pgHost}:5432/pg`;
 
-  const cala = await CalaLedger.connect({ pgCon, outbox: { enabled: true } })
+  const cala = await CalaLedger.connect({ pgCon, outbox: { enabled: true } });
   console.log("CalaLedger connected");
 
   const account = await cala.accounts().create({
     name: "MY NAME",
     code: "USERS_ONE",
     metadata: {
-      "something": "users",
-      "more": true
-    }
-  })
+      something: "users",
+      more: true,
+    },
+  });
   console.log("Account created", account.id());
   const account2 = await cala.accounts().create({
     name: "MY NAME",
     code: "USERS_TWO",
     metadata: {
-      "something": "users",
-      "more": true
-    }
-  })
+      something: "users",
+      more: true,
+    },
+  });
 
   console.log("Account created", account2.id());
 
@@ -36,9 +36,9 @@ const main = async () => {
   const journal = await cala.journals().create({
     name: "MY JOURNAL",
     description: "MY DESCRIPTION",
-  })
+  });
 
   console.log("Journal Created", journal.id());
-}
+};
 
-main()
+main();
