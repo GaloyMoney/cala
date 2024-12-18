fcf3556cd2821d940781d293867d7450c3f3d341
bodymindarts <justin@galoy.io>
2024-12-18 11:04:22 +0100

    chore: add CodeAlreadyExists to JournalError

--- cala-ledger/src/journal/error.rs
+++ cala-ledger/src/journal/error.rs
@@ -3,11 +3,26 @@ use thiserror::Error;
 #[derive(Error, Debug)]
 pub enum JournalError {
     #[error("JournalError - Sqlx: {0}")]
-    Sqlx(#[from] sqlx::Error),
+    Sqlx(sqlx::Error),
     #[error("JournalError - EsEntityError: {0}")]
     EsEntityError(es_entity::EsEntityError),
     #[error("JournalError - CursorDestructureError: {0}")]
     CursorDestructureError(#[from] es_entity::CursorDestructureError),
+    #[error("JournalError - code already exists")]
+    CodeAlreadyExists,
+}
+
+impl From<sqlx::Error> for JournalError {
+    fn from(error: sqlx::Error) -> Self {
+        if let Some(err) = error.as_database_error() {
+            if let Some(constraint) = err.constraint() {
+                if constraint.contains("code") {
+                    return Self::CodeAlreadyExists;
+                }
+            }
+        }
+        Self::Sqlx(error)
+    }
 }
 
 es_entity::from_es_entity_error!(JournalError);
