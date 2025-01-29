b9156f3c100f743de41b5cbbd3916af9e2ace75a
Vaibhav <thevaibhavdixit@gmail.com>
2025-01-29 22:04:42 +0530

    refactor: find for update in velocity

--- cala-ledger/src/velocity/balance/repo.rs
+++ cala-ledger/src/velocity/balance/repo.rs
@@ -1,5 +1,5 @@
 use es_entity::DbOp;
-use sqlx::{PgPool, QueryBuilder, Row};
+use sqlx::{PgPool, QueryBuilder};
 
 use std::collections::HashMap;
 
@@ -27,79 +27,123 @@ impl VelocityBalanceRepo {
             _pool: pool.clone(),
         }
     }
-
     pub async fn find_for_update(
         &self,
         op: &mut DbOp<'_>,
         keys: impl Iterator<Item = &VelocityBalanceKey>,
     ) -> Result<HashMap<VelocityBalanceKey, Option<BalanceSnapshot>>, VelocityError> {
-        let mut query_builder = QueryBuilder::new(
-            r#"
-            WITH inputs AS (
-              SELECT *
-              FROM (
-            "#,
-        );
-        query_builder.push_values(
-            keys,
-            |mut builder, (window, currency, journal_id, account_id, control_id, limit_id)| {
-                builder.push_bind(window);
-                builder.push_bind(currency.code());
-                builder.push_bind(journal_id);
-                builder.push_bind(account_id);
-                builder.push_bind(control_id);
-                builder.push_bind(limit_id);
-            },
-        );
-        query_builder.push(
-            r#"
-              ) AS v(partition_window, currency, journal_id, account_id, velocity_control_id, velocity_limit_id)
-            ),
-            locked_balances AS (
-              SELECT b.partition_window, b.currency, b.journal_id, b.account_id, b.velocity_control_id, b.velocity_limit_id, b.latest_version
-              FROM cala_velocity_current_balances b
-              WHERE ((partition_window, currency, journal_id, account_id, velocity_control_id, velocity_limit_id) IN (SELECT * FROM inputs))
-              FOR UPDATE
-            )
-            SELECT i.partition_window, i.currency, i.journal_id, i.account_id, i.velocity_control_id, i.velocity_limit_id, h.values
-            FROM inputs i
-            LEFT JOIN locked_balances b
-            ON i.partition_window = b.partition_window
-              AND i.currency = b.currency
-              AND i.journal_id = b.journal_id
-              AND i.account_id = b.account_id
-              AND i.velocity_control_id = b.velocity_control_id
-              AND i.velocity_limit_id = b.velocity_limit_id
-            LEFT JOIN cala_velocity_balance_history h
-            ON b.partition_window = h.partition_window
-              AND b.currency = h.currency
-              AND b.journal_id = h.journal_id
-              AND b.account_id = h.account_id
-              AND b.velocity_control_id = h.velocity_control_id
-              AND b.velocity_limit_id = h.velocity_limit_id
-              AND b.latest_version = h.version
-        "#
-        );
-        let query = query_builder.build();
-        let rows = query.fetch_all(&mut **op.tx()).await?;
+        let (windows, currencies, journal_ids, account_ids, control_ids, limit_ids) = keys.fold(
+        (Vec::new(), Vec::new(), Vec::new(), Vec::new(), Vec::new(), Vec::new()),
+        |(mut windows, mut currencies, mut journal_ids, mut account_ids, mut control_ids, mut limit_ids), &(ref window, ref currency, journal_id, account_id, control_id, limit_id)| {
+            windows.push(window.inner().clone());
+            currencies.push(currency.code());
+            journal_ids.push(journal_id);
+            account_ids.push(account_id);
+            control_ids.push(control_id);
+            limit_ids.push(limit_id);
+            (windows, currencies, journal_ids, account_ids, control_ids, limit_ids)
+        }
+    );
+
+        sqlx::query!(
+        r#"
+        SELECT pg_advisory_xact_lock(hashtext(concat(
+            partition_window::text,
+            currency,
+            journal_id::text,
+            account_id::text,
+            velocity_control_id::text,
+            velocity_limit_id::text
+        )))
+        FROM UNNEST(
+            $1::jsonb[], 
+            $2::text[], 
+            $3::uuid[], 
+            $4::uuid[], 
+            $5::uuid[], 
+            $6::uuid[]
+        )
+        AS v(partition_window, currency, journal_id, account_id, velocity_control_id, velocity_limit_id)
+        "#,
+        &windows[..],
+        &currencies as &[&str],
+        &journal_ids  as &[JournalId],
+        &account_ids as &[AccountId],
+        &control_ids  as &[VelocityControlId],
+        &limit_ids  as &[VelocityLimitId],
+    )
+    .execute(&mut **op.tx())
+    .await?;
+
+        let rows = sqlx::query!(
+        r#"
+        WITH inputs AS (
+          SELECT *
+          FROM UNNEST(
+            $1::jsonb[], 
+            $2::text[], 
+            $3::uuid[], 
+            $4::uuid[], 
+            $5::uuid[], 
+            $6::uuid[]
+          )
+          AS v(partition_window, currency, journal_id, account_id, velocity_control_id, velocity_limit_id)
+        ),
+        locked_balances AS (
+          SELECT b.partition_window, b.currency, b.journal_id, b.account_id, b.velocity_control_id, b.velocity_limit_id, b.latest_version
+          FROM cala_velocity_current_balances b
+          WHERE (partition_window, currency, journal_id, account_id, velocity_control_id, velocity_limit_id) IN (SELECT partition_window, currency, journal_id, account_id, velocity_control_id, velocity_limit_id FROM inputs)
+          FOR UPDATE
+        )
+        SELECT 
+            i.partition_window as "partition_window!: serde_json::Value", 
+            i.currency as "currency!", 
+            i.journal_id as "journal_id!: JournalId", 
+            i.account_id as "account_id!: AccountId", 
+            i.velocity_control_id as "velocity_control_id!: VelocityControlId", 
+            i.velocity_limit_id as "velocity_limit_id!: VelocityLimitId",
+            h.values as "values?: serde_json::Value"
+        FROM inputs i
+        LEFT JOIN locked_balances b
+        ON i.partition_window = b.partition_window
+          AND i.currency = b.currency
+          AND i.journal_id = b.journal_id
+          AND i.account_id = b.account_id
+          AND i.velocity_control_id = b.velocity_control_id
+          AND i.velocity_limit_id = b.velocity_limit_id
+        LEFT JOIN cala_velocity_balance_history h
+        ON b.partition_window = h.partition_window
+          AND b.currency = h.currency
+          AND b.journal_id = h.journal_id
+          AND b.account_id = h.account_id
+          AND b.velocity_control_id = h.velocity_control_id
+          AND b.velocity_limit_id = h.velocity_limit_id
+          AND b.latest_version = h.version
+        "#,
+        &windows[..],
+        &currencies as &[&str],
+        &journal_ids  as &[JournalId],
+        &account_ids as &[AccountId],
+        &control_ids  as &[VelocityControlId],
+        &limit_ids  as &[VelocityLimitId],
+    )
+    .fetch_all(&mut **op.tx())
+    .await?;
 
         let mut ret = HashMap::new();
         for row in rows {
-            let values: Option<serde_json::Value> = row.get("values");
-            let snapshot = values.map(|v| {
+            let snapshot = row.values.map(|v| {
                 serde_json::from_value::<BalanceSnapshot>(v)
                     .expect("Failed to deserialize balance snapshot")
             });
             ret.insert(
                 (
-                    row.get("partition_window"),
-                    row.get::<&str, _>("currency")
-                        .parse()
-                        .expect("Could not parse currency"),
-                    row.get("journal_id"),
-                    row.get("account_id"),
-                    row.get("velocity_control_id"),
-                    row.get("velocity_limit_id"),
+                    Window::from(row.partition_window),
+                    row.currency.parse().expect("Could not parse currency"),
+                    row.journal_id,
+                    row.account_id,
+                    row.velocity_control_id,
+                    row.velocity_limit_id,
                 ),
                 snapshot,
             );
