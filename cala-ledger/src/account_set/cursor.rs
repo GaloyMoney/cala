28bb7d803ccd694dcea23ec8c258324217488aa3
Arvin <17693119+vindard@users.noreply.github.com>
2025-03-19 13:46:41 -0400

    fix: add id to 'AccountSetMembersCursor' (#353)
    * fix: add id to 'AccountSetMemberCursor'
    
    * chore: remove unused 'AccountSetMemberCursor'
    
    * chore: add Copy to AccountSetMemberId
    
    * fix: add id to 'AccountSetMembersCursor'
    
    * chore: .sqlx updates

--- cala-ledger/src/account_set/cursor.rs
+++ cala-ledger/src/account_set/cursor.rs
@@ -1,7 +1,4 @@
-use cala_types::{
-    account_set::{AccountSetMember, AccountSetValues},
-    primitives::AccountSetId,
-};
+use cala_types::{account_set::AccountSetValues, primitives::AccountSetId};
 use serde::{Deserialize, Serialize};
 
 #[derive(Debug, Serialize, Deserialize)]
@@ -15,19 +12,6 @@ impl From<&AccountSetValues> for AccountSetByNameCursor {
         Self {
             name: values.name.clone(),
             id: values.id,
-        }
-    }
-}
-
-#[derive(Debug, Serialize, Deserialize)]
-pub struct AccountSetMemberCursor {
-    pub member_created_at: chrono::DateTime<chrono::Utc>,
-}
-
-impl From<AccountSetMember> for AccountSetMemberCursor {
-    fn from(member: AccountSetMember) -> Self {
-        Self {
-            member_created_at: member.created_at,
         }
     }
 }
