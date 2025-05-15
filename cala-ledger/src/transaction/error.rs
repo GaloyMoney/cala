c7ea01227bb6f969419ec86deeb45d7d1404d626
Justin Carter <justin@galoy.io>
2025-05-15 11:25:58 +0200

    chore: add value to TransactionError::DuplicateXxx (#413)

--- cala-ledger/src/transaction/error.rs
+++ cala-ledger/src/transaction/error.rs
@@ -1,3 +1,4 @@
+use regex::Regex;
 use thiserror::Error;
 
 use cala_types::primitives::TransactionId;
@@ -14,26 +15,40 @@ pub enum TransactionError {
     EsEntityError(es_entity::EsEntityError),
     #[error("TransactionError - CursorDestructureError: {0}")]
     CursorDestructureError(#[from] es_entity::CursorDestructureError),
-    #[error("TransactionError - DuplicateExternalId: external_id already exists")]
-    DuplicateExternalId,
-    #[error("TransactionError - DuplicateId: id already exists")]
-    DuplicateId,
+    #[error("TransactionError - DuplicateExternalId: external_id '{0}' already exists")]
+    DuplicateExternalId(String),
+    #[error("TransactionError - DuplicateId: id '{0}' already exists")]
+    DuplicateId(String),
 }
 
 impl From<sqlx::Error> for TransactionError {
     fn from(e: sqlx::Error) -> Self {
         match e {
             sqlx::Error::Database(ref err) if err.is_unique_violation() => {
-                let Some(constraint) = err.constraint() else {
+                if err.constraint().is_none() {
                     return Self::Sqlx(e);
                 };
-                if constraint.contains("external_id") {
-                    Self::DuplicateExternalId
-                } else if constraint.contains("id") {
-                    Self::DuplicateId
-                } else {
-                    Self::Sqlx(e)
+
+                let detail = err
+                    .downcast_ref::<sqlx::postgres::PgDatabaseError>()
+                    .detail();
+
+                if let Some(detail_msg) = detail {
+                    let re = Regex::new(r"Key \((?P<field>[^)]+)\)=\((?P<value>[^)]+)\)").unwrap();
+
+                    if let Some(caps) = re.captures(detail_msg) {
+                        let field = &caps["field"];
+                        let value = &caps["value"];
+
+                        match field {
+                            "external_id" => return Self::DuplicateExternalId(value.to_string()),
+                            "id" => return Self::DuplicateId(value.to_string()),
+                            _ => {}
+                        }
+                    }
                 }
+
+                Self::Sqlx(e)
             }
             e => Self::Sqlx(e),
         }
