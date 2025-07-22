da36bbee64d546acd0b9ea5a0f924583bbe1ee3d
Arunabha <arunabhadhal04@gmail.com>
2025-07-22 19:07:29 +0530

    refactor: update build configuration

--- cala-cel-parser/build.rs
+++ cala-cel-parser/build.rs
@@ -1,5 +1,4 @@
 fn main() {
-    // LALRPOP parser is pre-generated and checked into source control
-    // to work around issues with code generation in Nix builds
-    // lalrpop::process_root().unwrap();
+    lalrpop::process_root().unwrap();
 }
+
--- cala-cel-parser/src/lib.rs
+++ cala-cel-parser/src/lib.rs
@@ -1,8 +1,10 @@
 #![cfg_attr(feature = "fail-on-warnings", deny(warnings))]
 #![cfg_attr(feature = "fail-on-warnings", deny(clippy::all))]
 
+use lalrpop_util::lalrpop_mod;
+
 pub mod ast;
-#[allow(clippy::all)]
-pub mod parser;
 
 pub use ast::*;
+
+lalrpop_mod!(#[allow(clippy::all)] pub parser, "/cel.rs");
