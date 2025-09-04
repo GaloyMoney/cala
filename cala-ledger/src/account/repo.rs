8f6b37168f02e847757d825a142f597018ab63bc
vindard <17693119+vindard@users.noreply.github.com>
2025-09-04 10:25:00 -0400

    feat: add new cache_latest_values functions to accounts repo

--- cala-ledger/src/account/repo.rs
+++ cala-ledger/src/account/repo.rs
@@ -1,7 +1,7 @@
 use es_entity::*;
 use sqlx::PgPool;
 
-use crate::primitives::{DataSourceId, DebitOrCredit};
+use crate::primitives::{AccountId, DataSourceId, DebitOrCredit};
 
 use super::{entity::*, error::AccountError};
 
@@ -68,6 +68,60 @@ impl AccountRepo {
         .execute(op.as_executor())
         .await?;
         self.persist_events(op, account.events_mut()).await?;
+        Ok(())
+    }
+
+    pub async fn update_latest_values_in_op(
+        &self,
+        op: &mut impl es_entity::AtomicOperation,
+        latest_values: impl Into<AccountValuesForContext>,
+    ) -> Result<(), AccountError> {
+        let latest_values: AccountValuesForContext = latest_values.into();
+        let account_id = latest_values.id;
+
+        sqlx::query!(
+            r#"UPDATE cala_accounts
+            SET latest_values = $2
+            WHERE id = $1"#,
+            account_id as AccountId,
+            serde_json::to_value(latest_values).expect("Failed to serialize account values"),
+        )
+        .execute(op.as_executor())
+        .await?;
+        Ok(())
+    }
+
+    pub async fn update_all_latest_values_in_op(
+        &self,
+        op: &mut impl es_entity::AtomicOperation,
+        latest_values_list: Vec<impl Into<AccountValuesForContext>>,
+    ) -> Result<(), AccountError> {
+        if latest_values_list.is_empty() {
+            return Ok(());
+        }
+
+        let mut id_collection: Vec<AccountId> = Vec::new();
+        let mut latest_values_collection: Vec<serde_json::Value> = Vec::new();
+
+        for latest_values in latest_values_list {
+            let latest_values: AccountValuesForContext = latest_values.into();
+            id_collection.push(latest_values.id);
+            latest_values_collection.push(
+                serde_json::to_value(latest_values).expect("Failed to serialize account values"),
+            );
+        }
+
+        sqlx::query!(
+            r#"UPDATE cala_accounts
+            SET latest_values = unnested.latest_values
+            FROM UNNEST($1::UUID[], $2::JSONB[]) AS unnested(id, latest_values)
+            WHERE cala_accounts.id = unnested.id"#,
+            &id_collection as &[AccountId],
+            &latest_values_collection,
+        )
+        .execute(op.as_executor())
+        .await?;
+
         Ok(())
     }
 }
