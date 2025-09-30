93b8fb1b93e97c770e3ae2e8e493a33de187c632
pablo <pablomartincalvo@gmail.com>
2025-09-30 06:58:06 +0200

    fix: use  instead of docker

--- bats/helpers.bash
+++ bats/helpers.bash
@@ -8,10 +8,10 @@ SERVER_PID_FILE="${CALA_HOME}/server-pid"
 EXAMPLE_PID_FILE="${CALA_HOME}/rust-example-pid"
 
 reset_pg() {
-  docker exec "${COMPOSE_PROJECT_NAME}-server-pg-1" psql $PG_CON -c "DROP SCHEMA public CASCADE"
-  docker exec "${COMPOSE_PROJECT_NAME}-server-pg-1" psql $PG_CON -c "CREATE SCHEMA public"
-  docker exec "${COMPOSE_PROJECT_NAME}-examples-pg-1" psql $PG_CON -c "DROP SCHEMA public CASCADE"
-  docker exec "${COMPOSE_PROJECT_NAME}-examples-pg-1" psql $PG_CON -c "CREATE SCHEMA public"
+  $DOCKER_ENGINE exec "${COMPOSE_PROJECT_NAME}-server-pg-1" psql $PG_CON -c "DROP SCHEMA public CASCADE"
+  $DOCKER_ENGINE exec "${COMPOSE_PROJECT_NAME}-server-pg-1" psql $PG_CON -c "CREATE SCHEMA public"
+  $DOCKER_ENGINE exec "${COMPOSE_PROJECT_NAME}-examples-pg-1" psql $PG_CON -c "DROP SCHEMA public CASCADE"
+  $DOCKER_ENGINE exec "${COMPOSE_PROJECT_NAME}-examples-pg-1" psql $PG_CON -c "CREATE SCHEMA public"
 }
 
 server_cmd() {
